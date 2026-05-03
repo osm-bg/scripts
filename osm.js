04c7619cb920387638cbd3b49bc29a027b8c0913
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2026-05-03 10:45:38 +0300

    add debug mode and escape quotes

--- osm.js
+++ osm.js
@@ -1,4 +1,6 @@
 import dotenv from 'dotenv';
+import fs from 'fs';
+
 dotenv.config();
 
 const OSM_API_BASE = 'https://api.openstreetmap.org/api/0.6';
@@ -30,10 +32,10 @@ function generate_osm_change_xml(changeset_id, elements) {
         }
         xml_parts.push(`<modify>`);
         if(element.type === 'node') {
-            xml_parts.push(`<node id="${element.id}" version="${element.version++}" changeset="${changeset_id}" visible="true" lat="${element.lat}" lon="${element.lon}">`);
+            xml_parts.push(`<node id="${element.id}" version="${element.version}" changeset="${changeset_id}" visible="true" lat="${element.lat}" lon="${element.lon}">`);
         }
         else {
-            xml_parts.push(`<${element.type} id="${element.id}" version="${element.version++}" changeset="${changeset_id}" visible="true">`);
+            xml_parts.push(`<${element.type} id="${element.id}" version="${element.version}" changeset="${changeset_id}" visible="true">`);
         }
         if(element.type === 'way' && Array.isArray(element.nodes)) {
             for(const node_id of element.nodes) {
@@ -46,7 +48,7 @@ function generate_osm_change_xml(changeset_id, elements) {
             }
         }
         for(const [key, value] of Object.entries(element.tags)) {
-            xml_parts.push(`<tag k="${key}" v="${value}" />`);
+            xml_parts.push(`<tag k="${key}" v="${value.replace(/"/g, '&quot;')}" />`);
         }
         xml_parts.push(`</${element.type}>`);
         xml_parts.push(`</modify>`);
@@ -55,7 +57,13 @@ function generate_osm_change_xml(changeset_id, elements) {
     return xml_parts.join('\n');
 }
 
-export function upload_elements_to_osm(changeset_tags, elements) {
+export function upload_elements_to_osm(changeset_tags, elements, debug = false) {
+    if(debug) {
+        console.log('Debug mode enabled - not uploading to OSM');
+        console.log('Changeset tags:', changeset_tags);
+        fs.writeFileSync(`./output/changeset_${Math.random().toString(36).substring(2, 8)}.txt`, generate_osm_change_xml('debug', elements), 'utf-8');
+        return Promise.resolve();
+    }
     var changeset_id;
     return fetch(`${OSM_API_BASE}/changeset/create`, {
         method: 'PUT',
@@ -64,8 +72,13 @@ export function upload_elements_to_osm(changeset_tags, elements) {
         },
         body: generate_osm_xml_for_changeset(changeset_tags)
     })
-    .then(res => res.text())
-    .then(loc_changeset_id => {
+    .then(async (res) => {
+        if(!res.ok) {
+            console.error('Failed to create changeset');
+            console.error(res.status, res.statusText, await res.text());
+            return Promise.reject(new Error('Failed to create changeset'));
+        }
+        const loc_changeset_id = await res.text();
         console.log(`Created changeset ${loc_changeset_id} for uploading ${elements.length} modified elements`);
         changeset_id = loc_changeset_id;
         return fetch(`${OSM_API_BASE}/changeset/${changeset_id}/upload`, {
@@ -77,7 +90,12 @@ export function upload_elements_to_osm(changeset_tags, elements) {
             body: generate_osm_change_xml(changeset_id, elements)
         })
     })
-    .then(() => {
+    .then(async (res) => {
+        if(!res.ok) {
+            console.error(`Failed to upload changeset ${changeset_id}`);
+            console.error(res.status, res.statusText, await res.text());
+            return Promise.reject(new Error(`Failed to upload changeset ${changeset_id}`));
+        }
         console.log(`Uploaded changeset ${changeset_id}, now closing it`);
         return fetch(`${OSM_API_BASE}/changeset/${changeset_id}/close`, {
             method: 'PUT',
